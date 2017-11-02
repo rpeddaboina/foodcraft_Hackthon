cb17cdcb14ea491eddc5989cf9e17e8a3315382a
StraferInferno <akshay9al@gmail.com>
2017-11-01 23:08:44 -0700

    service intergrations and clean up

--- foodcraftui/src/app/register-course/register-course.service.ts
+++ foodcraftui/src/app/register-course/register-course.service.ts
@@ -6,22 +6,22 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RegisterCourseService {
-  resourceUrl= "/api";
+  resourceUrl= "http://ec2-18-216-137-158.us-east-2.compute.amazonaws.com:9099/enroll";
 
   constructor(private http: HttpClient){ }
 
   storeRegistrationDetails(formData:any):Observable<any>{
-    // return this.http.post(this.resourceUrl,formData,{ observe:'response'})
-    //   .map((response)=>{
-    //     console.log(response);
-    //     return response.body
-    //   }).catch((error:any)=>{
-    //       if(error.status < 400 || error.status ===500 ){
-    //         return Observable.throw(new Error(error.status));
-    //       }
-    //   });
+    return this.http.post(this.resourceUrl,formData,{ observe:'response'})
+      .map((response)=>{
+          console.log(response);
+          return { 'message' : response.body }
+      }).catch((error:any)=>{
+        if(error.status < 400 || error.status ===500 ){
+          return Observable.throw(new Error(error.status));
+        }
+      });
 
-    return Observable.create(observer => {
+    /*return Observable.create(observer => {
       const randomTime = 1000 + (Math.random() * 2000);
       setTimeout(
         function() {
@@ -29,8 +29,8 @@ export class RegisterCourseService {
           observer.complete();
         }
         , randomTime);
-      });
-    }
+      });*/
+  }
 
 
 
